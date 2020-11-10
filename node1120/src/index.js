e0bd0bf558474b85fa0afe5a3059b4b42e6919bc
wasirobin <69293514+wasirobin@users.noreply.github.com>
2020-11-10 20:09:10 +0800

    Update index.js

--- node1120/src/index.js
+++ node1120/src/index.js
@@ -1,31 +1,34 @@
-require("dotenv").config()
-
+//引入express套件
 const express = require("express")
-
+//引入連接database的檔案
+const db = require(__dirname + "/course_db_connect")
+//導入開放網域模組
 const cors = require("cors")
-const db = require(__dirname + "/db_connect2")
-
-const bodyParser = require("body-parser")
+//建立web server物件
 const app = express()
+const bodyParser = require("body-parser")
 
 app.use(cors())
-app.use(express.urlencoded({ extended: false }))
+//會解析req過來的JSON字串
 app.use(express.json())
-// get方法
-app.get("/get-db", (req, res) => {
-  db.query("SELECT * FROM course_list ").then(([results]) => {
-    res.json(results)
-  })
-})
+//urlencoded是exoress的body-parser功能
+app.use(express.urlencoded({ extended: false }))
 
-const courseRoutes=require('./routes/courseRoutes')
-app.use('/course',courseRoutes)
 
+const courseRoutes = require("./routers/courseRoutes")
+app.use("/course", courseRoutes)
+
+
+// 連線資料庫get資料方法
+// app.get("/get-db", (req, res) => {
+//   db.query("SELECT * FROM `course_list` ").then(([results]) => {
+//     res.json(results)
+//   })
+// })
+
+
+//讓public暴露
 app.use(express.static(__dirname + "/../public"))
-
-app.use((req, res) => {
-  res.type("text/plain").status(404).send("找不到網頁")
-})
 
 app.listen(3000, () => {
   console.log("伺服器已啟動...")
